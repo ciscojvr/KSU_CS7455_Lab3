c9efac68dc308887bc73ae7c1511fe5685a1ed63
Francisco Ozuna <ozuna252@hotmail.com>
2020-06-08 23:21:13 -0400

    Added click listener on login button

--- app/src/main/java/com/example/lab3/LoginActivity.java
+++ app/src/main/java/com/example/lab3/LoginActivity.java
@@ -3,11 +3,16 @@ package com.example.lab3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "Main Activity"; // Using TAG constant for logging
     EditText username;
     EditText password;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,19 @@ public class LoginActivity extends AppCompatActivity {
 
         username = (EditText) findViewById(R.id.editText_username);
         password = (EditText) findViewById(R.id.editText_password);
+        loginButton = (Button) findViewById(R.id.button_login);
+
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (username.getText().toString().equals("cs7455") && password.getText().toString().equals("123456")) {
+                    Log.d(TAG, "Login successful!");
+                }  else {
+                    Log.d(TAG, "Incorrect login!");
+                }
+            }
+        });
     }
 }
